e53f10e092c572436fc6a20ab3f52b0fa56bc3c7
John Mitromaras <mitromarasg96@gmail.com>
2018-05-11 00:05:05 +0300

    cleaning and commenting

--- MySet.py
+++ MySet.py
@@ -7,8 +7,11 @@ class MySet(set):
 
     def add(self, path):
         found = False
+        # for each of the paths we have so far discovered(visited or not)
         for p in self.paths:
+            # if the path's starting and ending nodes coincide with p's ones
             if path == p:
+                # if path is cheapest than p -> add it
                 if path < p:
                     self.paths.remove(p)
                     self.paths.append(path)
@@ -17,22 +20,34 @@ class MySet(set):
                     found = True
                     break
 
+        # if path's and p's starting and ending nodes do not coincide with p's -> add it
         if not(found):
             self.paths.append(path)
 
     def pop(self):
+        # the last node of the path we are about to pop is now considered visited
         self.visited.append(self.min.last)
+
+        # return the next unvisited cheapest path
         return self.min
 
     def set_min(self):
+        # get the unvisited paths
         not_visited_paths = list(filter(lambda x: not(x.last in self.visited), self.paths))
         if not not_visited_paths:
             return
+
+        # find the cheapest of them
         self.min = min(not_visited_paths)
 
     def has_next(self):
+        # get the last nodes of all paths in our path list
         last_nodes = set(map(lambda x: x.last, self.paths))
+
+        # check if the last_nodes set is a subset of the visited set
         diff = last_nodes.difference(set(self.visited))
+
+        # if it is then there are no more unvisited paths
         return not(diff.__len__() == 0)
 
     def print(self):
--- web_crawler.py
+++ web_crawler.py
@@ -10,43 +10,65 @@ from urllib import parse
 base_url = "http://127.0.0.1:3117"
 first_page = "a.html"
 
-# NOTE program output(under consideration): tree of all best routes to final pages(more outputs to be added)
-# NOTE                                      dead links, and (probably) all of their routes
+# TODO find and print dead links
 
 # TODO create an xml(or something) file that prettily presents the best route tree
 
-# NOTE The program must decide which route to choose to go with(namely which link to request next from each page)
-# NOTE                    ^partially done
-# NOTE despite the fact that in the end it will scan every possible route
-
-# TODO create a heuristic(or something) function that determines why a route is preferable over another.
-# TODO It will possibly be used the location(internal/external) of the page. More to be added for a better discrimination
+# TODO improve the heuristic
 
 
 def create_pages(urls):
+    """Gets a list of urls and creates Page objects.
+       These nodes represent the nodes of the graph."""
     pages = []
     for url in urls:
         pages.append(Page(url, base_url))
     return pages
 
 
+# create a custom composite data structure that implements some of the algorithm logic
+# and keeps track of each needed data structure
 collection = MySet()
+
+# create the first page/node
 aPage = Page(first_page, base_url)
+
+# create a path starting from the first page
 path = Path(aPage)
 
+# add to the data structure which will update its contents
 collection.add(path)
+
+# find the next min path that will be chosen
 collection.set_min()
 
+# while there are still unvisited paths
 while collection.has_next():
+
+    # get the next unvisited cheapest path
     path = collection.pop()
+
+    # get its last node
     node = path.last
 
+    # get its neighbors
     raw_neighbors = create_pages(node.links)
+
+    # get its unvisited neighbors
     neighbors = list(filter(lambda x: not(x in collection.visited), raw_neighbors))
+
+    # for each neighbor
     for n in neighbors:
+        # copy the base path so we can create a new path for each neighbor
         new_path = path.copy()
+
+        # create a path for the neighbor
         new_path.add(n)
+
+        # let the collection decide whether to add it or not
         collection.add(new_path)
+
+    # find the next min path
     collection.set_min()
 
 for p in collection.paths:
